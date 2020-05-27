81caed0a6f3fc00f784e372ca6ee623a49f0e0cd
ewooooo <nuw577@naver.com>
2020-05-27 17:38:44 +0900

    200527_Button_MainButton add - python

--- MainProcess/myButton.py
+++ MainProcess/myButton.py
@@ -1,25 +1,109 @@
 #from Main import LoopSystem
+import RPi.GPIO as GPIO
 import time
 
 class MyButton:
-    def __init__(self, obj):
-        self.main = obj
-        print("makebutton")
+    LONGCLICKTIME = 1
+    DOUBLECLICKTIME = 0.5
+    ONECLICKTIME = 0.05
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)    # set GPIO25 as input (button)  
+        GPIO.setup(17, GPIO.OUT)   # set GPIO24 as an output (LED)  
+
+        #self.main = obj
+        #print("makebutton")
+
     def checkButton(self):
-        while True:
-            self.main.listTest.append("ButtonCheck")
-            time.sleep(1)
+        # self.main.listTest.append("ButtonCheck")
+        # time.sleep(1)
+        try:
+            count =0
+            doubleClickCount = 0
+            doubleClickON = False
+            longlock = False
+            while True:
+                if GPIO.input(22):
+                    GPIO.output(17, 1)
+                    if count == 0:
+                        count = time.time_ns()
+                        if(doubleClickCount != 0):
+                            delaytime = (time.time_ns() - doubleClickCount)/1000000000
+                            if delaytime <= self.DOUBLECLICKTIME:
+                                doubleClickON = True
+                    else :
+                        if not longlock :
+                            delaytime = (time.time_ns() - count)/1000000000
+                            if delaytime >= self.LONGCLICKTIME:
+                                print(delaytime)
+                                self.longClick()
+                                doubleClickCount = 0
+                                doubleClickON = False
+                                longlock = True
+                            
+
+                else:
+                    GPIO.output(17,0)
+                    if longlock :
+                        count = 0
+                        longlock = False
+                    if(count != 0):
+                        delaytime = (time.time_ns() - count)/1000000000
+                        # if delaytime >= self.LONGCLICKTIME:
+                        #     print(delaytime)
+                        #     self.longClick()
+                        #     if doubleClickON :
+                        #         count = 0
+                        #         doubleClickCount = 0
+                        #         doubleClickON = False
+                        if delaytime > self.ONECLICKTIME:
+                            if doubleClickON :
+                                print(delaytime)
+                                self.doubleClick()
+                                count = 0
+                                doubleClickON = False
+                                doubleClickCount = 0
+                            else:
+                                doubleClickCount = time.time_ns()
+                        count = 0
+                    else :
+                        if doubleClickCount != 0:
+                            delaytime = (time.time_ns() - doubleClickCount)/1000000000
+                            if delaytime > self.DOUBLECLICKTIME:
+                                print(delaytime)
+                                self.oneClick()
+                                count = 0
+                                doubleClickCount = 0
+                                doubleClickON = False
+        finally :
+            GPIO.cleanup()
 
     def oneClick(self):
-        #현재재생중 버스 번호 받아 userbuslist에 저장
         print("oneClick")
 
     def doubleClick(self):
-
         print("doubleClick")
 
     def longClick(self):
         print("longClick")
+
     def wakeUpTest(self):
         print("wakeUp")
         return True
+
+
+
+if __name__ == "__main__":
+    print("start")
+    # doubleClickCount = time.time_ns()
+    # count = 1
+    # while(True):
+    #     delaytime = (time.time_ns() - doubleClickCount)/1000000000
+    #     if delaytime > count :
+    #         count = count + 1
+    #         print(delaytime)
+    #         print(count)
+    
+    b= MyButton()
+    b.checkButton()
