5071d1f0323b077ddacd53de18bf894db1bf9a45
kyoung-sin <62052908+kyoung-sin@users.noreply.github.com>
2020-05-29 00:53:09 +0900

    Update led.py

--- LED/led.py
+++ LED/led.py
@@ -24,7 +24,7 @@ def demo(n, block_orientation, rotate, inreverse):
        text(draw, (11, 20), msg[3], fill="white")
     print(msg1)
     time.sleep(4)
-
+#test
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='matrix_demo arguments',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
