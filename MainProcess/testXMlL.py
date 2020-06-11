86ebb66f1861e512ced1b16d840900cfcf27221f
ewooooo <nuw577@naver.com>
2020-06-11 10:27:45 +0900

    200611_main_ing

---
+++ MainProcess/testXMlL.py
@@ -0,0 +1,76 @@
+text = '''
+<response>
+    <comMsgHeader/>
+    <msgHeader>
+        <queryTime>2020-06-08 22:32:58.813</queryTime>
+        <resultCode>0</resultCode>
+        <resultMessage>정상적으로 처리되었습니다.</resultMessage>
+    </msgHeader>
+    <msgBody>
+        <busArrivalList>
+            <flag>PASS</flag>
+            <locationNo1>3</locationNo1>
+            <locationNo2/>
+            <lowPlate1>0</lowPlate1>
+            <lowPlate2>0</lowPlate2>
+            <plateNo1>경기70바2552</plateNo1>
+            <plateNo2/>
+            <predictTime1>10</predictTime1>
+            <predictTime2/>
+            <remainSeatCnt1>-1</remainSeatCnt1>
+            <remainSeatCnt2>-1</remainSeatCnt2>
+            <routeId>200000008</routeId>
+            <staOrder>16</staOrder>
+            <stationId>203000165</stationId>
+        </busArrivalList>
+        <busArrivalList>
+            <flag>PASS</flag>
+            <locationNo1>1</locationNo1>
+            <locationNo2>10</locationNo2>
+            <lowPlate1>0</lowPlate1>
+            <lowPlate2>0</lowPlate2>
+            <plateNo1>경기70바5467</plateNo1>
+            <plateNo2>경기70바5649</plateNo2>
+            <predictTime1>1</predictTime1>
+            <predictTime2>21</predictTime2>
+            <remainSeatCnt1>13</remainSeatCnt1>
+            <remainSeatCnt2>29</remainSeatCnt2>
+            <routeId>200000112</routeId>
+            <staOrder>56</staOrder>
+            <stationId>203000165</stationId>
+        </busArrivalList>
+        <busArrivalList>
+            <flag>PASS</flag>
+            <locationNo1>3</locationNo1>
+            <locationNo2>7</locationNo2>
+            <lowPlate1>0</lowPlate1>
+            <lowPlate2>0</lowPlate2>
+            <plateNo1>경기70바5477</plateNo1>
+            <plateNo2>경기70바3695</plateNo2>
+            <predictTime1>5</predictTime1>
+            <predictTime2>15</predictTime2>
+            <remainSeatCnt1>10</remainSeatCnt1>
+            <remainSeatCnt2>12</remainSeatCnt2>
+            <routeId>200000119</routeId>
+            <staOrder>51</staOrder>
+            <stationId>203000165</stationId>
+        </busArrivalList>
+        <busArrivalList>
+            <flag>PASS</flag>
+            <locationNo1>2</locationNo1>
+            <locationNo2>23</locationNo2>
+            <lowPlate1>0</lowPlate1>
+            <lowPlate2>1</lowPlate2>
+            <plateNo1>경기70바1825</plateNo1>
+            <plateNo2>경기70바1474</plateNo2>
+            <predictTime1>8</predictTime1>
+            <predictTime2>31</predictTime2>
+            <remainSeatCnt1>-1</remainSeatCnt1>
+            <remainSeatCnt2>-1</remainSeatCnt2>
+            <routeId>200000146</routeId>
+            <staOrder>56</staOrder>
+            <stationId>203000165</stationId>
+        </busArrivalList>
+    </msgBody>
+</response>'''
+'''
