47c59c3a4428a3d23dd07d4c95830cdd5dfbf9d4
ewooooo <nuw577@naver.com>
2020-06-16 02:44:56 +0900

    200616_main_ing

--- MainProcess/LED.py
+++ MainProcess/LED.py
@@ -14,7 +14,7 @@ class LED:
         serial = spi(port=0, device=0, gpio=noop())
         self.device = max7219(serial, width=32, height=32, block_orientation=-90, rotate=1)
     def SET_LED(self,number):
-        self.device.show()
+
         number = number.replace('-', 'l')
 
         testlen = len(number)
@@ -101,10 +101,10 @@ class LED:
                     text(draw, (24, 22), number[5], fill="white", font=proportional(TINY_FONT))
                 else:
                     text(draw, (21, 22), number[5], fill="white")
-
+        self.device.show()
     def OFF_LED(self):
-        self.device.clear()
-        #self.device.hide()
+        #self.device.clear()
+        self.device.hide()
 
 
 
