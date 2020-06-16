56dbb323fa4d8a4a02d91be251faa9acab49b54e
ewooooo <nuw577@naver.com>
2020-06-17 02:42:56 +0900

    200617_test_main_sucess

--- MainProcess/LED.py
+++ MainProcess/LED.py
@@ -11,13 +11,11 @@ from luma.core.legacy.font import proportional,TINY_FONT
 
 class LED:
     def __init__(self):
-        self.device = max7219(serial, width=32, height=32, block_orientation=-90, rotate=1)
         serial = spi(port=0, device=0, gpio=noop())
-        self.device.show()
-    # def reset(self):
-    #     serial = spi(port=0, device=0, gpio=noop())
-    #     self.device = max7219(serial, width=32, height=32, block_orientation=-90, rotate=1)
+        self.device = max7219(serial, width=32, height=32, block_orientation=-90, rotate=1)
+
     def SET_LED(self,number):
+        self.device.show()
         number = number.replace('-', 'l')
 
         testlen = len(number)
