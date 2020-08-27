446810da5f9cf2373071c47ee54ec71ca40e545c
남은우 <nuw577@naver.com>
2020-08-27 17:36:11 +0900

    ======================== git refactoring =======================

--- TensorFlow/Test/hello.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-
-import tensorflow as tf
-
-hello = tf.constant("hello world")
-
-print(hello)
-print(hello.numpy()) # Tensor flow(Cuda, cuDNN) Test code
