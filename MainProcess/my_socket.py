66631efd4a51983bbed05e7779c15b6ac2361508
ewooooo <nuw577@naver.com>
2020-09-23 01:11:44 +0900

    v1.0.3 iss #6 socket refactory

--- MainProcess/my_socket.py
+++ MainProcess/my_socket.py
@@ -1,7 +1,7 @@
 import socket
 import time
 
-class mySocket:
+class baseSocket:
     def __init__(self,HOST,PORT):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         while True:
@@ -15,17 +15,28 @@ class mySocket:
                 continue
 
     def Send_Recv(self,command):
+        
+        self.s.send(command.encode())
+        mode = self.s.recv(10).decode("UTF-8")
+
+        return mode
+    
+
+class mySocket(baseSocket):
+
+    def Send_Recv(self,command):
         #[2, (0_버스 발견못함 1_버스 발견됨 2_버스 정차함 -1_대기 시간초과(버싀나감)), 버스번호]
         buffer = '_________'
         buffer = command + buffer
-        self.s.send(buffer[0:10].encode())
-
-        mode = self.s.recv(10).decode("UTF-8")
+        mode = super().Send_Recv(buffer[0:10])
         mode = mode[0:9]
         mode = mode.replace('_','')
         SList = mode.split('|')
         print(SList)
+
         return SList
+
+
 
 
 
@@ -34,7 +45,7 @@ class mySocket:
 if __name__ == "__main__":
     print("start")
 
-    st= mySocket("192.168.0.32",12345)
+    st= mySocket("127.0.0.1",12345)
     while True:
         a = input("입력 : ")
         print(st.Send_Recv(a))
